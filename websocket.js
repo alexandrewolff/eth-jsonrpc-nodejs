7a671883ec86159ac1efa836670d5a7c0fa39d34
Alexandre Wolff <alexandrewlff@gmail.com>
2021-07-30 16:52:14 +0200

    Add comments to websocket file

--- websocket.js
+++ websocket.js
@@ -6,32 +6,46 @@ const ws = new WebSocket(process.env.WS_PROVIDER);
 let subId;
 
 ws.on('open', () => {
-  const sub = JSON.stringify({
+  /**
+   * Subscription
+   */
+  const payload = JSON.stringify({
     jsonrpc: '2.0',
     id: 1,
     method: 'eth_subscribe',
     params: ['newHeads'],
   });
-  ws.send(sub);
-});
+  ws.send(payload);
 
-setTimeout(() => {
-  const unsub = JSON.stringify({
-    jsonrpc: '2.0',
-    id: 1,
-    method: 'eth_unsubscribe',
-    params: [subId],
+  /**
+   * Unsubscription after 10s
+   */
+  setTimeout(() => {
+    const payload = JSON.stringify({
+      jsonrpc: '2.0',
+      id: 1,
+      method: 'eth_unsubscribe',
+      params: [subId],
+    });
+    ws.send(payload);
+  }, 10000);
+  
+  /**
+   * Listener
+   */
+  ws.on('message', (msg) => {
+    const data = JSON.parse(msg);
+    console.log(data);
+    
+    /**
+     * result field is populated with subscription id on subscription
+     * result field is true on unsubscription
+     */
+    if (data.result === true) {
+      ws.close();
+    } else if (data.result !== undefined) {
+      subId = data.result;
+    }
   });
-  ws.send(unsub);
-}, 10000);
-
-ws.on('message', (msg) => {
-  const data = JSON.parse(msg);
-  console.log(data);
-
-  if (data.result === true) {
-    ws.close();
-  } else if (data.result !== undefined) {
-    subId = data.result;
-  }
 });
+
